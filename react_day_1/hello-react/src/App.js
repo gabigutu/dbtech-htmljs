dd7c5a7ad212b94a110207a357a2d8d41f799457
Cipppp <pirvuciprian37@gmail.com>
2022-05-12 19:38:43 +0300

    Footer and Header added

---
+++ react_day_1/hello-react/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header/>
+        
+
+        <Footer/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_day_1/hello-react/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// import React, { Component } from 'react';
+
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Footer</h1>
+            </div>
+        );
+    }
+}
+
+export default Footer;
