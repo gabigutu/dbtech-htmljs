bc2e5578e0e877cf83057b91c19e637213482be1
Wiliam Toader <wiliamtoader@protonmail.com>
2022-05-12 23:26:31 +0300

    added react demo using tsx

---
+++ react_day_1/typescript-react/src/components/SubComp.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class MyComponent extends Component {
+    static propTypes: {};
+    render() {
+        return (
+            <div>
+                Test
+            </div>
+        );
+    }
+}
+
+MyComponent.propTypes = {};
+
+export default MyComponent;
